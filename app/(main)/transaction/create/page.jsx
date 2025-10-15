4e977ad7c18c31193050499ce047170f1a7a0a99
vathdev <vthien562004@gmail.com>
2025-10-15 23:29:13 +0700

    Fix searchParams

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -4,8 +4,9 @@ import { AddTransactionForm } from "../_components/transaction-form";
 import { getTransaction } from "@/actions/transaction";
 
 export default async function AddTransactionPage({ searchParams }) {
+  const params = await searchParams;
   const accounts = await getUserAccounts();
-  const editId = searchParams?.edit;
+  const editId = params?.edit;
 
   let initialData = null;
   if (editId) {
@@ -16,7 +17,7 @@ export default async function AddTransactionPage({ searchParams }) {
   return (
     <div className="max-w-3xl mx-auto px-5">
       <div className="flex justify-center md:justify-normal mb-8">
-        <h1 className="text-5xl gradient-title ">Add Transaction</h1>
+        <h1 className="text-5xl gradient-title">Add Transaction</h1>
       </div>
       <AddTransactionForm
         accounts={accounts}
